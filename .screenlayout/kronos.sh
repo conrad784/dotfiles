8a3b2d065e087da9330463bf4251d5619e106eed
Conrad Sachweh <conrad@csachweh.de>
2019-01-30 11:49:45 +0100

    kronos layout

---
+++ .screenlayout/kronos.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+       --output HDMI-1 --off \
+       --output HDMI-2 --off \
+       --output DP-1 --off \
+       --output DP-2 --off \
+       --output DP-2-1 --off \
+       --output DP-2-2 --off \
+       --output DP-2-3 --off
+
